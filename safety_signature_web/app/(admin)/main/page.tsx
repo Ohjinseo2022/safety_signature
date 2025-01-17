c1bffe998a9f2d1737425c434e7c239317736572
Ohjinseo2022 <dhwlstj@gmail.com>
2025-01-17 10:53:47 +0900

    관리자화면 react -> nextJs 로 프레임워크 변경

---
+++ safety_signature_web/app/(admin)/main/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+
+interface pageProps{
+
+}
+
+const page = ({}:pageProps) => {
+    return(
+        <>
+        해위잉
+        </>
+    );
+}
+
+export default page;
---
+++ safety_signature_web/next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: false,
+  
+  //타입스크립트 적용
+  typescript: {
+    ignoreBuildErrors: false,
+    tsconfigPath: "tsconfig.json",
+  },
+  distDir: ".next",
+  async rewrites() {
+    //redirects  rewrites
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
